c21496489094dbdd121f157610d6e16471b339fb
realniceping <86911372+realniceping@users.noreply.github.com>
2022-09-26 13:59:55 +0500

    laba4

---
+++ laba4/thread/Program.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Threading;
+using System.IO;
+
+namespace thread
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("x1>>");
+            string x1s = Console.ReadLine();
+            Console.Write("s1>>");
+            string s1s = Console.ReadLine();
+            double x1 = Convert.ToDouble(x1s);
+            double s1 = Convert.ToDouble(s1s);
+
+            Console.Write("x2>>"); //start
+            string x2s = Console.ReadLine();
+            Console.Write("s2>>"); // step
+            string s2s = Console.ReadLine();
+            Console.Write("d2(ms)>>"); // latency
+            string d2s = Console.ReadLine();
+            Console.Write("t2(s)>>"); // time
+            string t2s = Console.ReadLine();
+
+            double x2 = Convert.ToDouble(x2s);
+            double s2 = Convert.ToDouble(s2s);
+            int d2 = Convert.ToInt32(d2s);
+            int t2 = Convert.ToInt32(t2s);
+
+            Thread ParentThread = new Thread(delegate () { FirstThreader.main(x1, s1); });
+            ParentThread.Start();
+            SecondThread secondThreadObject = new SecondThread();
+            Thread SecondThread = new Thread(delegate () { secondThreadObject.main(x2, s2, d2, t2); });
+            SecondThread.Start();
+        }
+    }
+
+    public class FirstThreader
+    {
+        public static async void main(double x1, double s1)
+        {
+
+
+
+            using StreamWriter file = new("thread1.dat");
+            for (int i = 0; i < 100; i++)
+            {
+                await file.WriteLineAsync(DateTime.Now + " value X:" + x1 + " value Y: " + Math.Log(x1));
+                x1 = x1 + s1;
+                Thread.Sleep(500);
+            }
+
+        }
+    }
+
+    public class SecondThread
+    {
+        public async void main(double x2, double s2, int d2, int t2)
+        {
+
+
+            using StreamWriter file = new("thread2.dat");
+            DateTime current = DateTime.Now;
+            long currentSecond = current.Second;
+
+            long finshSecod = currentSecond + t2;
+
+            while (currentSecond < finshSecod)
+            {
+                await file.WriteLineAsync(DateTime.Now + " value X:" + x2 + " value Y: " + 1 / x2);
+                x2 = x2 + s2;
+                current = DateTime.Now;
+                currentSecond = current.Second;
+                Thread.Sleep(d2);
+            }
+
+        }
+
+    }
+
+}
--- osi/child/Program.cs
+++ osi/child/Program.cs
@@ -4,88 +4,102 @@ using System.Collections.Specialized;
 
 public class ChildProcess
 {
-  public static void Main()
-  {
-    try
+    public static void Main()
     {
-      string s;
-      
-      Console.WriteLine("Child process started");
+        try
+        {
+            string s;
 
-      // Получаем текущий процесс
-      Process p = Process.GetCurrentProcess();
-      Console.WriteLine(p);
-      Console.WriteLine("Current process geted");
+            Console.WriteLine("Child process started");
 
-      List<string> value = new List<string>();
-      value.Add(Environment.GetEnvironmentVariable("function"));
-      Console.WriteLine(value[0]);
-      value.Add(Environment.GetEnvironmentVariable("min"));
-      Console.WriteLine(value[1]);
-      value.Add(Environment.GetEnvironmentVariable("max"));
-      Console.WriteLine(value[2]);
-      value.Add(Environment.GetEnvironmentVariable("step"));
-      Console.WriteLine(value[3]);
+            // Получаем текущий процесс
+            Process p = Process.GetCurrentProcess();
+            Console.WriteLine(p);
+            Console.WriteLine("Current process geted");
 
-    System.Console.WriteLine(value.Count);
-    foreach(string v in value){
-        System.Console.WriteLine(v);
+            List<string> value = new List<string>();
+            value.Add(Environment.GetEnvironmentVariable("function"));
+
+            value.Add(Environment.GetEnvironmentVariable("min"));
+
+            value.Add(Environment.GetEnvironmentVariable("max"));
+
+            value.Add(Environment.GetEnvironmentVariable("step"));
+
+
+            System.Console.WriteLine(value.Count);
+            foreach (string v in value)
+            {
+                System.Console.WriteLine(v);
+            }
+
+            int function = -1;
+            double min = 0;
+            double max = 10;
+            double step = 0.5;
+
+            if (value[0] != null && value[1] != null && value[2] != null && value[3] != null)
+            {
+                function = Convert.ToInt32(value[0]);
+                min = Convert.ToDouble(value[1]);
+                max = Convert.ToDouble(value[2]);
+                step = Convert.ToDouble(value[3]);
+            }
+
+            Console.WriteLine("_______________");
+
+            if (function == 1)
+            {
+                for (double i = min; i < max; i = i + step)
+                {
+                    Console.WriteLine("| " + i + " | " + Math.Sin(i) + " |");
+                }
+            }
+
+            if (function == 2)
+            {
+                for (double i = min; i < max; i = i + step)
+                {
+                    Console.WriteLine("| " + i + " | " + Math.Cos(i) + " |");
+                }
+            }
+
+            if (function == 3)
+            {
+                for (double i = min; i < max; i = i + step)
+                {
+                    Console.WriteLine("| " + i + " | " + i * i + " |");
+                }
+            }
+
+            if (function == 4)
+            {
+                for (double i = min; i < max; i = i + step)
+                {
+                    Console.WriteLine("| " + i + " | " + Math.Pow(2.71, i) + " |");
+                }
+            }
+
+            if (function == 5)
+            {
+                for (double i = min; i < max; i = i + step)
+                {
+                    Console.WriteLine("| " + i + " | " + Math.Log(i) + " |");
+                }
+            }
+
+            Console.WriteLine("_______________");
+
+            // Для каждого ключа в блоке окружения 
+            // выводим его значение
+
+
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e.Message);
+        }
+
     }
 
-      int function = -1;
-      double min = 0;
-      double max  = 10;
-      double step = 0.5;
-
-      if(value[0] != null){
-        function = Convert.ToInt32(value[0]);
-        min = Convert.ToDouble(value[1]);
-        max = Convert.ToDouble(value[2]);
-        step = Convert.ToDouble(value[3]);
-      }
-      
-      Console.WriteLine("_______________");
-      
-        if(function == 1){
-          for(double i = min; i < max; i = i + step){
-            Console.WriteLine("| " + i + " | " + Math.Sin(i) + " |" );
-          }
-        }
-
-        if(function == 2){
-          for(double i = min; i < max; i = i + step){
-            Console.WriteLine("| " + i + " | " + Math.Cos(i) + " |" );
-          }
-        }
-
-        if(function == 3){ 
-          for(double i = min; i < max; i = i + step){
-            Console.WriteLine("| " + i + " | " + i * i + " |" );
-          }
-        }
-
-        if(function == 4){ 
-          for(double i = min; i < max; i = i + step){
-            Console.WriteLine("| " + i + " | " + Math.Pow(2.71, i) + " |" );
-          }
-        }
-
-        if(function == 5){ 
-          for(double i = min; i < max; i = i + step){
-            Console.WriteLine("| " + i + " | " + Math.Log(i) + " |" );
-          }
-        }
-       
-      Console.WriteLine("_______________");
-
-      // Для каждого ключа в блоке окружения 
-      // выводим его значение
-      
-
-    } catch (Exception e) {
-      Console.WriteLine(e.Message);
-    }    
-
-  }
-  
 }
