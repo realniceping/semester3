4da44a6c8075eb31609f135f189e983a487a7750
realniceping <arsenya.osipenko@mail.ru>
2022-10-03 20:41:43 +0500

    laba5

---
+++ linklist/Item.cs
@@ -0,0 +1,22 @@
+using System;
+
+namespace linklist;
+
+class Item
+{
+
+    public Item previos;
+
+    public Item next;
+
+    public int value;
+
+    public Item(int value){
+
+        this.previos = null;
+        this.next = null;
+        this.value = value;
+    
+    }
+
+}
